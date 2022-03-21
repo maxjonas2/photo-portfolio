18e883015df06f039741cfec5df3e24a12ffac5a
maxjonas2 <maxjonas2@hotmail.com>
2022-03-21 02:49:54 -0300

    Added side menu and scripting

--- public/script.js
+++ public/script.js
@@ -4,6 +4,27 @@ const path = "FOTO1.jpg";
 const HEADER_HIDDEN_SCROLL_POINT = 300;
 const HERO = document.querySelector(".hero-container");
 
+const menu = document.getElementById("menu");
+const menuContainer = document.getElementById("menu-container");
+
+function openMenu() {
+  menu.parentElement.style.display = "block";
+  menu.parentElement.classList.add("shown");
+  menu.classList.add("shown");
+}
+
+function closeMenu() {
+  menu.parentElement.style.display = "none";
+  menu.parentElement.classList.remove("shown");
+  menu.classList.remove("shown");
+}
+
+menuContainer.addEventListener("click", e => {
+  if (e.target === menuContainer) {
+    closeMenu();
+  }
+});
+
 let scrollMap = 0;
 
 function moveBackground() {
