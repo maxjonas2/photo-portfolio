9d7427705c689553e0cda3db6bbedf23ec644872
maxjonas2 <maxjonas2@hotmail.com>
2022-05-02 00:41:34 -0300

    Vanilla lightbox setup

--- public/galleries/galleries-app/src/components/Lightbox.js
+++ public/galleries/galleries-app/src/components/Lightbox.js
@@ -1,34 +1,71 @@
-import React from "react";
-import path from "path";
+import React, { useState } from "react";
+import std from "styled-components";
+import Loader from "../assets/svg/Loader";
+import close from "../assets/svg/close.svg";
+import arrow from "../assets/svg/arrow.svg";
 
-export const Lightbox = () => {
+export const Lightbox = ({ currentImage, changeImage, setLightboxOpen }) => {
+  const [imageLoading, setImageLoading] = useState(false);
+
   return (
-    <div className="lightbox-overlay">
-      <button
-        className="btn btn-lightbox-close centered-items"
-        id="btn-lightbox-close"
-      >
-        <img src={require("../assets/svg/close.svg")} alt="" />
-      </button>
-      <div className="lightbox-container">
-        <div className="lightbox-image-container"></div>
+    <Fill className="centered-items">
+      <LightboxContainer className="centered-items">
+        <picture>
+          <LightboxImage
+            src={currentImage.url.replace("small", "large")}
+            alt="Image"
+            onLoad={() => setImageLoading(false)}
+            loading={imageLoading}
+          />
+        </picture>
+
         <div className="lightbox-controls horizontal-items">
-          <button className="btn lightbox-btn" id="lightbox-btn-back">
-            <img
-              className="image-svg"
-              src={require("../assets/svg/arrows.svg")}
-              alt=""
-            />
+          <button
+            onClick={() => {
+              setImageLoading(true);
+              changeImage("previous");
+            }}
+            className="btn lightbox-btn"
+          >
+            <img src={arrow} className="invert-horizontal" />
           </button>
-          <button className="btn lightbox-btn" id="lightbox-btn-next">
-            <img
-              className="image-svg"
-              src={require("../assets/svg/arrows.svg")}
-              alt=""
-            />
+          <button
+            onClick={() => {
+              setImageLoading(true);
+              changeImage("next");
+            }}
+            className="btn lightbox-btn"
+          >
+            <img src={arrow} />
           </button>
         </div>
-      </div>
-    </div>
+      </LightboxContainer>
+      {imageLoading ? (
+        <Fill className="centered-items" darkness={0.4}>
+          <Loader />
+        </Fill>
+      ) : null}
+      <button
+        className="btn btn-lightbox-close"
+        onClick={() => setLightboxOpen(false)}
+      >
+        <img src={close} alt="Close" />
+      </button>
+    </Fill>
   );
 };
+
+const Fill = std.div`
+  position: fixed;
+  inset: 0;
+  z-index: 9999;
+  background-color: ${props =>
+    props.darkness ? `rgba(0,0,0,${props.darkness})` : "rgba(0,0,0,.85)"};
+  margin: 0px !important;
+`;
+
+const LightboxContainer = std.div``;
+
+const LightboxImage = std.img`
+  max-height: 90vh;
+`;
